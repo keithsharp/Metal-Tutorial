7d1c53450441079032f16fac4e4acc74ae9820e3
Keith Sharp <kms@passback.co.uk>
2019-05-02 09:13:56 +0100

    Initial Commit

---
+++ Metal Tutorial/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Metal Tutorial
+//
+//  Created by Keith Sharp on 02/05/2019.
+//  Copyright © 2019 Passback Systems. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Metal Tutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Metal Tutorial
+//
+//  Created by Keith Sharp on 02/05/2019.
+//  Copyright © 2019 Passback Systems. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
